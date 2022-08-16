8d6c64813f33840b686e1b096f2e1dfa56278402
Jeff Wheeler <jeff@1wheeler.com>
2022-08-16 14:30:05 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -11,10 +11,12 @@ def patched_m2r2_setup(app):
     try:
         return current_m2r2_setup(app)
     except (AttributeError):
-        app.add_source_suffix(".md", "markdown")
+        app.add_source_suffix(".md", ".rst", "markdown")
         app.add_source_parser(m2r2.M2RParser)
     return dict(
         version=m2r2.__version__, parallel_read_safe=True, parallel_write_safe=True,
     )
 
 m2r2.setup = patched_m2r2_setup
+
+source_suffix = ['.rst', '.md']
